869c609a2271ac06c73df035a921a1dc3301de22
Barna Barabasi <bbarabasi@endava.com>
2019-04-17 07:42:46 +0300

    Generating points

---
+++ TSP/TSP/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public double Distance(Point a, Point b)
+        {
+            double distX = a.X - b.X;
+            double distY = a.Y - b.Y;
+            return Math.Sqrt(distX * distX + distY * distY);
+        }
+    }
+}
--- TSP/TSP/Program.cs
+++ TSP/TSP/Program.cs
@@ -10,6 +10,22 @@ namespace TSP
     {
         static void Main(string[] args)
         {
+            List<Point> points = GeneratePoints(10);
+        }
+
+        private static List<Point> GeneratePoints(int size)
+        {
+            List<Point> points = new List<Point>();
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                points.Add(new Point
+                {
+                    X = rand.NextDouble() * 100,
+                    Y = rand.NextDouble() * 100
+                });
+            }
+            return points;
         }
     }
 }
